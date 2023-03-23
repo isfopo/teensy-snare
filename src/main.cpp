4fa67e5747921cce5f3fe3a3fd9f27aba217dc1a
Isaac Poole <isaac@switcherstudio.com>
2023-03-22 20:28:51 -0400

    adds logic to trigger snare drum

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
+#include <hellodrum.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define MIDI_CHANNEL 0
+
+byte SNARE[11] = {
+    90, // sensitivity (1-100)
+    10, // threshold (1-100)
+    5,  // scan time (1-100)
+    40, // mask time (1-100)
+    20, // rim sensitivity (1-100)
+    10, // rim threshold (1-100)
+    3,  // curve type (0-4)
+    38, // note (0-127)
+    40, // alternate snare
+    37, // note of rim (0-127)
+    39  // alterate rim
+};
+
+HelloDrum snare(11, 10);
+
+void setup()
+{
+  snare.setCurve(SNARE[6]);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  snare.dualPiezo(SNARE[0], SNARE[1], SNARE[2], SNARE[3], SNARE[4], SNARE[5]);
+
+  // Snare
+
+  if (snare.hit == true)
+  {
+    usbMIDI.sendNoteOn(SNARE[8], snare.velocity, MIDI_CHANNEL);
+    usbMIDI.sendNoteOff(SNARE[8], 0, MIDI_CHANNEL);
+  }
+  else if (snare.hitRim == true)
+  {
+    usbMIDI.sendNoteOn(SNARE[10], snare.velocity, MIDI_CHANNEL);
+    usbMIDI.sendNoteOff(SNARE[10], 0, MIDI_CHANNEL);
+  }
 }
