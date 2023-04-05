e317748d0aa66945e6257b7e12c60d51c630939a
Isaac Poole <isaac@switcherstudio.com>
2023-04-04 21:54:18 -0400

    increases sensitivity

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@
 #define MIDI_CHANNEL 0
 
 byte SNARE[9] = {
-    90, // sensitivity (1-100)
-    10, // threshold (1-100)
-    5,  // scan time (1-100)
-    40, // mask time (1-100)
-    20, // rim sensitivity (1-100)
-    10, // rim threshold (1-100)
-    3,  // curve type (0-4)
-    38, // note (0-127)
-    37, // note of rim (0-127)
+    100, // sensitivity (1-100)
+    10,  // threshold (1-100)
+    5,   // scan time (1-100)
+    40,  // mask time (1-100)
+    20,  // rim sensitivity (1-100)
+    10,  // rim threshold (1-100)
+    3,   // curve type (0-4)
+    38,  // note (0-127)
+    37,  // note of rim (0-127)
 };
 
 HelloDrum snare(15, 14);
